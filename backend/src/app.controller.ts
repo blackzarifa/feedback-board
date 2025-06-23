4692cf287892f1b15e297efe58c2a924b97d615b
blackzarifa <rafaelirades@gmail.com>
2025-06-23 20:13:58 -0300

    feat health endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,4 +9,12 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('health')
+  async checkHealth() {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
